af3af64ef4d0b1bc964c5c907bd4f05569971ff3
DimejiAre <olardi2@gmail.com>
2019-08-22 12:49:09 +0100

    create objects and tests for Instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,3 +25,29 @@ class Instructor extends Person{
         return `${student.name} receives a perfect score on ${subject}`
     }
 }
+
+let gabe = new Instructor({
+    name: 'Gabe',
+    age: '42',
+    location: 'Seattle',
+    speciality: 'Front-end',
+    favLanguage: 'Javascript',
+    catchPhrase: 'I need my serotonin!!',
+});
+
+console.log(gabe);
+console.log(gabe.demo('javascript'));
+console.log(gabe.grade({name: 'Isaac'}, 'javascript'));
+
+let josh = new Instructor({
+    name: 'Josh',
+    age: '37',
+    location: 'Chicago',
+    speciality: 'Back-end',
+    favLanguage: 'Python',
+    catchPhrase: 'Let\'s get this show on the road!!',
+});
+
+console.log(josh);
+console.log(josh.demo('python'));
+console.log(josh.grade({name: 'Peter'}, 'python'));
